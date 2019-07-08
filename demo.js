aa91817559ee852dc271fc7e108b91dd0a9716c6
ylm <yingleiming@outlook.com>
2019-07-08 13:19:23 +0800

    。。。

---
+++ demo.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// 导入http模块:
+var http = require('http');
+
+// 创建http server，并传入回调函数:
+http.createServer(function (request, response) {
+    // 回调函数接收request和response对象,
+    // 获得HTTP请求的method和url:
+    console.log(request.method + ': ' + request.url);
+    // 将HTTP响应200写入response, 同时设置Content-Type: text/html:
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    // 将HTTP响应的HTML内容写入response:
+    response.end('<h1>Hello world!</h1>');
+}).listen(8080);
+
+console.log('Server is running at http://127.0.0.1:8080/');
+
